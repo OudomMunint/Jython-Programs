06e56b582be60599517c8159acd9873ef54011c6
OudomMunint <108245473+OudomMunint@users.noreply.github.com>
2022-11-23 23:06:49 +1100

    editsd

--- img denoiser.py
+++ img denoiser.py
@@ -1,11 +1,8 @@
-#Author: Pannha Oudom Munint
-#date:8/7/2019
-#T1: open picture file, make file a picture and show the picture
-#T2: get the data about pixels
-#T3: Get the value of a pixel and return it
-#T4: pick a file then call the required function
+#Dev: Pannha Oudom Munint, L1 SWE
+#Opens a picture file, makes file a picture then showing the picture, gets the data about pixels, gets the value of a pixel and return it.
+#Use: pick a file then call the required function
 
-#T1
+
 def openShowPicture():
    #select file
    file1 = pickAFile()
@@ -16,14 +13,14 @@ def openShowPicture():
    #Display picture
    show(pic1)
    
-#T2
+
    #gets the pixel of the picture and shows in R,G,B
    pixel1 = getPixel(pic1, 32, 32)
    #Prints the value of the pixels
    #print pixel1
    #retuen the value of the pixels
    return(pixel1)
-#T3
+
 #get the value of a pixel and return it   
 def showPixelUser(fileName, x, y):
     myPicture = makePicture(fileName)
@@ -32,19 +29,19 @@ def showPixelUser(fileName, x, y):
     return(pixel1)
     
     
-#T4
+
 #Pick a file and call functions
 def getAFile():
    file = pickAFile()
    
-   #call T1: show picture
+   #call: show picture
    #showPicture(file)
    
-   #Call T2 = show value of pixel
+   #Call = show value of pixel
    #pixelValue = showPixel(file)
    #print(pixelValue)
    
-   #call T3: give the file, x and y locations
+   #call: give the file, x and y locations
    #user enters the x and y values foe that picture
    x = requestInteger("enter x of picture")
    y = requestInteger("enter y of picture")
